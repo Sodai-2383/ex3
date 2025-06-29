66290293d463b4b629f54eaeec4f269aa70e2b79
s1320146 <s1320146@u-aizu.ac.jp>
2025-06-29 21:49:07 +0900

    Dice game: initial revision

---
+++ dice.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+    
+public class dice{
+
+    public static void main(String[] args){
+	new dice();
+    }
+
+    dice(){
+	int a,b;
+	Random random = new Random();
+	
+	a=b=0;
+	
+	System.out.println("Rolling dice...");
+
+	while(a==0){
+	    a = random.nextInt(7);
+	}
+
+	while(b==0){
+	    b = random.nextInt(7);
+	}
+
+	System.out.println("Die 1: " + a);
+	System.out.println("Die 2: " + b);
+
+	System.out.println("Total value: " + (a+b));
+
+    }
+    
+}
